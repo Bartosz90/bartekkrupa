416381ba4938ee54d2256461b8c2f2b908f655fc
Bartek Krupa <krupa1.bk@gmail.com>
2019-10-03 23:23:53 +0200

    Updates

---
+++ precache-manifest.2e7611d51cc822c326d100fe379a6cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1414e2f62dbeb88a094f1e48ef5f5318",
+    "url": "/bartekkrupa/index.html"
+  },
+  {
+    "revision": "d34fc682413a772e3800",
+    "url": "/bartekkrupa/static/css/main.b27efb93.chunk.css"
+  },
+  {
+    "revision": "024206d7cf195ba137b0",
+    "url": "/bartekkrupa/static/js/2.1a03c50d.chunk.js"
+  },
+  {
+    "revision": "d34fc682413a772e3800",
+    "url": "/bartekkrupa/static/js/main.a689e5ba.chunk.js"
+  },
+  {
+    "revision": "3699326969a9d2b11b51",
+    "url": "/bartekkrupa/static/js/runtime-main.c6340741.js"
+  },
+  {
+    "revision": "ef8316873faefe0afe514e14670ce80d",
+    "url": "/bartekkrupa/static/media/logo.ef831687.png"
+  }
+]);
